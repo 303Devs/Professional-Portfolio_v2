26093738573cbccbdc22a06901bcaa0633fe8554
ChiElephant <anthony.merino@me.com>
2024-11-28 17:16:11 -0700

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <h1 className='font-semibold text-3xl'>
+        Welcome to the future home of 303Dev...
+      </h1>
+    </div>
+  )
+}
+
+export default Home
