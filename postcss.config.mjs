3ccf328f683dad1f89edb2fab16256745d844cb1
Anthony Merino <anthony.merino@icloud.com>
2025-04-10 01:08:42 -0600

    upgrading tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,14 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
 
-export default config;
+// export default config;
+
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  }
+}
